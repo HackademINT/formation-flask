6add96ca4efa4f1be2401ec11dfb3aca19969b82
Olivier DULCY <olivier.dulcy@gmail.com>
2018-12-05 19:20:11 +0100

    Renammed. Add routes

---
+++ 2_Routes/main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return 'Subpath %s' % subpath
+
+if __name__ == "__main__":
+    app.run()
